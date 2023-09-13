75f7ab525809a503d42f76c6f209cabff3d5a4c6
Aman Nayyar <aman.nayyar21@gmail.com>
2023-09-13 16:50:55 +0530

    Create Coupon View Functionality Implemented

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -5,24 +5,44 @@ using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
-	public class CouponController : Controller
-	{
-		private readonly ICouponService _couponService;
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
 
-		public CouponController(ICouponService couponService)
+        public CouponController(ICouponService couponService)
         {
-			_couponService = couponService;
+            _couponService = couponService;
         }
         public async Task<IActionResult> CouponIndex()
-		{
-			List<CouponDto>? list = new();
-			ResponseDto? response = await _couponService.GetAllCouponsAsync();
-			if (response != null && response.IsSuccess) 
-			{
-			list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
-			}
+        {
+            List<CouponDto>? list = new();
+            ResponseDto? response = await _couponService.GetAllCouponsAsync();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
 
-			return View(list);
-		}
-	}
+            return View(list);
+        }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponsAsync(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+            return View(model);
+        }
+
+    }
 }
