3b14f504d651e0f87ef6fe6f55e2dee79b173c6a
Aman Nayyar <aman.nayyar21@gmail.com>
2023-09-25 12:19:44 +0530

    Sign in a user in .NET Identity

--- Mango.Services.AuthAPI/Program.cs
+++ Mango.Services.AuthAPI/Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 builder.Services.AddScoped<IAuthService,AuthService>();
 
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
