cf03a8ef29fae1967defdeaec5b2fee3ec209e67
Aman Nayyar <aman.nayyar21@gmail.com>
2023-11-06 08:24:01 +0530

    Filter in Action

--- Mango.Web/wwwroot/js/order.js
+++ Mango.Web/wwwroot/js/order.js
@@ -1,13 +1,30 @@
 ﻿var dataTable;
 
 $(document).ready(function () {
-    loadDataTable();
-})
+    var url = window.location.search;
+    console.log(url);
+    if (url.includes("approved")) {
+        loadDataTable("approved");
+    }
+    else {
+        if (url.includes("readyforpickup")) {
+            loadDataTable("readyforpickup");
+        }
+        else {
+            if (url.includes("cancelled")) {
+                loadDataTable("cancelled");
+            }
+            else {
+                loadDataTable("all");
+            }
+        }
+    }
+});
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
         order: [[0, 'desc']],
-        "ajax": { url: "/Order/GetAll" },
+        "ajax": { url: "/order/getall?status=" + status },        
         "columns": [
             { data: 'orderHeaderId', "width": "5%" },
             { data: 'email', "width": "25%" },
